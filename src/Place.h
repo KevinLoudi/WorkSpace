9c6ad11d04ec9bdb56276db81aeafeb16b885a95
KevinLoudi <zhugengyuloudi@gmail.com>
2016-11-07 09:35:29 +0800

    restructure base class

--- src/Place.h
+++ src/Place.h
@@ -2,28 +2,25 @@
 #define PLACE_H
 
 #include<string>
+#include<iostream>
 using namespace std;
+
+typedef unsigned long UINT;
 
 class Place
 {
+	private:
+		double _Latitude;
+		double _Longitude; 
+		double _High;
+	protected:
+		UINT  _PopulationDensity;
+		string _ProductType;
+		UINT _Area;	
 	public:
 		Place();
-		virtual double* getPosition() const
-		{return new double[2]{_latitude,_longitude};}
-		virtual int getPeople() const {return _inhabitants_num;}
-		virtual bool update_products(string &new_pro)=0;
-		virtual bool move_people(int _in_out_people)=0;
-		//Place(double lat,double lon,double high,int sc,string & pro, int num):
+		virtual void GetInfo()=0;
 		virtual ~Place();
-	protected:
-		virtual void inhabitants_growth_bytime(int timepassed)=0;
-	private:
-		double _latitude;
-		double _longitude; 
-		double _high;
-		int _scope;
-		string _products;
-		int _inhabitants_num;
 };
 
 #endif
