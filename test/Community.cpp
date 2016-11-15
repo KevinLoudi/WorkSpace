f92efde38a4cf9f384d55042141fbcde9ad90086
KevinLoudi <zhugengyuloudi@gmail.com>
2016-11-15 16:54:39 +0800

    make up

---
+++ test/Community.cpp
@@ -0,0 +1,52 @@
+#include "Community.h"
+#include <ctime>
+
+Person::Person()
+{
+   //random decided an age and gender
+  srand (time(NULL));
+  int age=(rand()%50); //[0,50] years old
+  GENDER gender=GENDER((rand()%2));
+  //initial class member
+  _age=age;
+  _gender=gender;
+}
+
+Person::~Person()
+{
+
+}
+
+inline ostream & Person::printInfo(ostream & ros) const
+{
+	return ros<<_gender<<" aged "<<_age<<endl;
+}
+
+inline Person* Person::clone() const
+{
+	//invoke copy constructor to assign a new
+	//need to be delete
+	return new Person(*this);
+}
+
+ERR_CODE Person::update(const Person & rPerson)
+{
+	this->_age=rPerson._age;
+	this->_gender=rPerson._gender;
+	return SUCCESS;
+}
+
+inline Judge Person::isSatisfied(const GENDER & gender,const USINT & age) const
+{
+	return Judge(((this->_age>age)&&(this->_gender==gender)));
+}
+
+Community::Community()
+{
+
+}
+
+Community::~Community()
+{
+	
+}
