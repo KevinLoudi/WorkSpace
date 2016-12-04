6cde242e8dccac760401fb0894eb89dd284cc492
KevinLoudi <zhugengyuloudi@gmail.com>
2016-12-04 15:46:25 +0800

    implement more sort algorithem, intend to employ functor to enable great-, less- comparison, but template block the way

--- algorithm/algorithm.h
+++ algorithm/algorithm.h
@@ -4,18 +4,25 @@
 
 namespace Algorithm
 {
-     template<class T>
+    /*template<class T>
+    typedef bool (*CMP)(const T& a, const T& b);
+    template<class T>
+    bool isless(const T& a, const T& b) {a<b;} */
+
+    template<class T>
      class Sort
      {
      public:
             Sort() {}
             ~Sort() {}
+            /*swap two elements*/
             void _swap(T &a, T &b)
             {
                 T tmp=a;
                 a=b;
                 b=tmp;
             }
+            /*print all elements in order*/
            std::ostream& print(std::ostream & ros, T *a, int n)
            {
                int ix=n;
@@ -28,26 +35,71 @@ namespace Algorithm
                return ros;
            }
 
+           /*select sort algorithm (data, data_size)*/
             void selectsort(T *a, int n)
            {
                 for(int i=0;i<n;i++)
                 {
                     int _min=i;
+                    //find the largest element
                     for(int j=i+1;j<n;j++)
                     {
                        if(a[j]<a[_min]) _min=j;
+                       //if(isless(a[j],a[_min])) _min=j;
                     }
+                    //swap the largest element with the first position in the unsorted region
                     _swap(a[i],a[_min]);
                 }
             }
 
+            /*bubble sort algorithm (data, data_size)*/
             void bubblesort(T* a, int n)
             {
                 for(int i=0; i<n; i++)
                     for(int j=i;j-1>=0&&a[j]<a[j-1];j--)
                     {
-                     _swap(a[j],a[j-1]);
+                      //swap each two elements if they have not in order
+                      _swap(a[j],a[j-1]);
                     }
+            }
+
+            /*insert sort*/
+            void insertsort(T *a, int n, int incre)
+            {
+                int i,j;
+                for(i=incre; i<n ; i+=incre)
+                {
+                    for(j=i;(j>=incre)&&(a[j]<a[j-incre]);j-=incre)
+                    {
+                       _swap(a[j],a[j-incre]);
+                    }
+                }
+            }
+
+            /*quick sort*/
+            void quicksort(T* a, int low, int high)
+            {
+                //select a standard
+                int pivot=a[(low+high)/2];
+                int left=low-1;
+                int right=high;
+
+                if(low>=high)
+                    return;
+
+                _swap(a[(low+high)/2],a[high]);
+                do
+                {
+                    while(a[++left]<pivot);
+                    while(right!=0&&a[--right]>pivot);
+                    _swap(a[left],a[right]);
+                }while(left<right);
+
+                _swap(a[left],a[right]);
+                _swap(a[left],a[high]);
+
+                quicksort(a,low, left-1);
+                quicksort(a,left+1,high);
             }
 
      };
--- project/test_in_codeblock/main.cpp
+++ project/test_in_codeblock/main.cpp
@@ -3,15 +3,16 @@
 #include "../../algorithm/algorithm.h"
 
 using namespace std;
-
 int main()
 {
     using namespace Algorithm;
     Sort<int> s;
     int a[]={12,15,78,96,53,47,19,44};
     s.print(cout,a,8);
-    //s.selectsort(a,8);
-    s.bubblesort(a,8);
+    //s.selectsort(a,8,);
+    //s.bubblesort(a,8);
+    //s.insertsort(a,8,1);
+    s.quicksort(a,0,7);
     s.print(cout,a,8);
     return 0;
 }
